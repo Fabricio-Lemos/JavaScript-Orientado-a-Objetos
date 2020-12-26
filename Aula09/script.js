f570f72613c8dd30393475d31cab6f1320b98f31
Fabricio-Lemos <fabriciosantana349@gmail.com>
2020-12-26 14:41:37 -0300

    Nova Aula

--- Aula09/script.js
+++ Aula09/script.js
@@ -1,55 +1,3 @@
-/*var pessoa = {
-    nome:'Fabricio',
-    _idade: '18',
-    _sexo: 'Masculino',
-    casado: false,
-    state: 'pristine',
-    get idade() {
-        this.state = 'dirty'
-        return this._idade
-    },
-    set idade(valor){
-        this._idade = valor
-    }
-}
-
-pessoa.idade = '19'
-
-Object.defineProperty(pessoa, 'idade', {
-    enumerable: false,
-    value: '20',
-    writable: false
-})
-
-Object.defineProperty(pessoa, 'nome', {
-    configurable: false,
-    value: 'Roberto',
-    writable: false
-})
-pessoa.idade = '19'
-
-console.log(pessoa)
-console.log(pessoa.propertyIsEnumerable('nome'))
-
-for (propriedade in pessoa){
-    console.log(propriedade + ': ' + pessoa[propriedade])
-}
-
-Object.defineProperty(pessoa, 'sexo', {
-    get: function(){
-        return this._sexo
-    },
-    set: function(value){
-        this._sexo = value
-    }
-})
-
-pessoa.sexo = 'Feminino'
-
-var sexo = pessoa.sexo
-
-console.log(sexo)*/
-
 var pessoa = {};
 
 Object.defineProperties(pessoa,{
@@ -88,8 +36,21 @@ Object.defineProperties(pessoa,{
         }
     }
 });
+Object.seal(pessoa)
+console.log(Object.isSealed(pessoa))
+
+/*Object.preventExtensions(pessoa)
+console.log(Object.isExtensible(pessoa))
+delete pessoa.nome
+pessoa.altura = '1,78m'
+
+console.log(pessoa)
+
+/*var atributos = Object.getOwnPropertyDescriptor(pessoa, 'nome');
 
 pessoa.nome = 'Maria'
 pessoa.sexo = 'Feminino'
 console.log(pessoa.nome)
-console.log(pessoa.sexo)
+console.log(pessoa.sexo);
+
+console.log(atributos)*/
