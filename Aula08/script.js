add86732663cab5b068ab656e0a99beaaee1926b
Fabricio-Lemos <fabriciosantana349@gmail.com>
2020-12-26 14:27:02 -0300

    Nova Aula

--- Aula08/script.js
+++ Aula08/script.js
@@ -1,7 +1,7 @@
-var pessoa = {
+/*var pessoa = {
     nome:'Fabricio',
     _idade: '18',
-    sexo: 'Masculino',
+    _sexo: 'Masculino',
     casado: false,
     state: 'pristine',
     get idade() {
@@ -34,3 +34,62 @@ console.log(pessoa.propertyIsEnumerable('nome'))
 for (propriedade in pessoa){
     console.log(propriedade + ': ' + pessoa[propriedade])
 }
+
+Object.defineProperty(pessoa, 'sexo', {
+    get: function(){
+        return this._sexo
+    },
+    set: function(value){
+        this._sexo = value
+    }
+})
+
+pessoa.sexo = 'Feminino'
+
+var sexo = pessoa.sexo
+
+console.log(sexo)*/
+
+var pessoa = {};
+
+Object.defineProperties(pessoa,{
+    _nome: {
+        value:'Fabricio',
+        enumerable: true,
+        configurable: false,
+        writable: true
+    },
+    _sexo: {
+        value:'Masculino',
+        enumerable: true,
+        configurable: true,
+        writable: true
+    },
+    _idade: {
+        value:'18',
+        enumerable: true,
+        configurable: true,
+        writable: true
+    },
+    nome: {
+        get: function(){
+            return this._nome;
+        },
+        set: function(value){
+            this._nome = value;
+        }
+    },
+    sexo: {
+        get: function(){
+            return this._sexo;
+        },
+        set: function(value){
+            this._sexo = value;
+        }
+    }
+});
+
+pessoa.nome = 'Maria'
+pessoa.sexo = 'Feminino'
+console.log(pessoa.nome)
+console.log(pessoa.sexo)
